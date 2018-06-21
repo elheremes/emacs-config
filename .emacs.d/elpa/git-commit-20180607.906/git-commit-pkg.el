45960997f65dd7954b71b0e0ce9750e912b401fe
Pedro Thiago <thiago_cutrim@hotmail.com>
2018-06-21 18:05:48 -0300

    Changed to labmint pc config

---
+++ .emacs.d/elpa/git-commit-20180607.906/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20180607.906" "Edit Git commit messages" '((emacs "25.1") (dash "20180413") (with-editor "20180414")) :commit "553fdb0b25e2f3d4cd1fe43867039222a2df3afc" :keywords '("git" "tools" "vc") :url "https://github.com/magit/magit")
